3f28a362670ebb51b5f840ca0693062697faf7e2
moh2099 <moh_2323@hotmail.com>
2020-05-16 21:26:03 +0300

    note: Thunk !?

--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,4 +1,13 @@
 export const createProject = (project) => {
+{/* 
+
+This works fine: 
+  return {
+    type: 'CREATE_PROJECT', project 
+  }
+But we are using it now beacuse we want to use Thunk to make async call to the DB before the action created or send to the reducer
+
+*/}
   return (dispatch, getState) => {
     // make async call to database
     dispatch({ type: 'CREATE_PROJECT', project });
